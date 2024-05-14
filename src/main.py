7df5414c66d6662c7cf8b3ef791767778e3cdd93
AlanF1511 <alanfelipe@uniplaclages.edu.br>
2024-05-14 17:46:17 -0300

    Ajustes de login
    Atualizei o layout e iniciei as novas pedidas do tio lu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, session;
 from settings import HOST, PORT, DEBUG
+import os;
 
 # import blueprint criado
 from mod_funcionario.funcionario import bp_funcionario
@@ -9,7 +10,18 @@ from mod_produto.produto import bp_produto
 from mod_login.login import bp_login
 from mod_erro.erro import bp_erro
 
+# Tratamento de exceções na aplicação
+from mod_erro.erro import bp_erro
+
 app = Flask(__name__)
+
+# gerando uma chave randômica para secret_key
+app.secret_key = os.urandom(12).hex()
+
+app.config.update(
+    SESSION_COOKIE_SAMESITE='None',
+    SESSION_COOKIE_SECURE='True'
+)
 
 # registro das rotas do blueprint
 app.register_blueprint(bp_funcionario)
@@ -18,6 +30,8 @@ app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_produto)
 app.register_blueprint(bp_login)
 app.register_blueprint(bp_erro)
+
+app.secret_key = os.urandom(12).hex()
 
 if __name__ == "__main__":
     """ Inicia o aplicativo WEB Flask """
